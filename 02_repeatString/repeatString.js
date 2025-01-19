94addf28b51e5bc8a3e3ae37882149d10a6d5274
osoriogeorge <osoriovgeorge@gmail.com>
2025-01-19 09:48:55 +0100

    Implement repeatString function to repeat a word multiple times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let result = "";
+  for (let i = 0; i < times; i++) {
+    result += word;
+  }
+  return result;
 };
 
+console.log(repeatString("Hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
