e96d218909863b1df00c0cd062ba65af0dcc6f2a
osoriogeorge <osoriovgeorge@gmail.com>
2025-01-20 09:08:58 +0100

    Implement sum function that takes 2 positive integers and returns the sum of every integer between them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,10 @@
-const sumAll = function() {
-
+const sumAll = function (n1, n2) {
+  let suma = 0;
+  for (let i = n1; i <= n2; i++) {
+    suma += i;
+  }
+  return suma;
 };
-
+console.log(sumAll(1, 4));
 // Do not edit below this line
 module.exports = sumAll;
