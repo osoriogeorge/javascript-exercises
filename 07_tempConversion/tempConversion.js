29865f00cd39ffe3e7480bcc29d23326c1de360c
osoriogeorge <osoriovgeorge@gmail.com>
2025-01-20 10:35:53 +0100

    Add function to convert degrees Fahrenheit to Celsius and Celsius to Fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,21 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (celsius) {
+  let celsiusDegrees = Math.round(((celsius - 32) * 5) / 9);
 
-const convertToFahrenheit = function() {
+  console.log("Celsius degrees is " + celsiusDegrees);
+  return celsiusDegrees;
 };
+convertToCelsius(32);
+
+const convertToFahrenheit = function (fahrenheit) {
+  let fahrenheitDegrees = Math.round(1.8 * fahrenheit + 32);
+
+  console.log("Fahrenheit degrees is " + fahrenheitDegrees);
+  return fahrenheitDegrees;
+};
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
