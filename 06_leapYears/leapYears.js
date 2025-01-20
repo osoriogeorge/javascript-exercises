530f07c3bfc603212120b4f96af8e9e009db15a4
osoriogeorge <osoriovgeorge@gmail.com>
2025-01-20 10:07:53 +0100

    Add function to determine if a given year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 == 0 && year % 400 == 0) {
+    console.log("year " + year + " is a Lap");
+  } else if (year % 100 == 0) {
+    console.log("year " + year + " is aLap");
+  } else {
+    console.log("year " + year + " is not a Lap");
+  }
 };
+//console.log();
+leapYears(2000);
+leapYears(1985);
 
 // Do not edit below this line
 module.exports = leapYears;
