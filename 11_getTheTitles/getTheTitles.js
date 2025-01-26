b3622adce82b8249659a3269d85c74ce7f05d83b
osoriogeorge <osoriovgeorge@gmail.com>
2025-01-26 15:34:52 +0100

    Mapping array to return the titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
-
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
+const getTheTitles = function () {
+  const titlesOfBooks = books.map((book) => book.title);
+  console.log(titlesOfBooks);
+  return titlesOfBooks;
 };
 
 // Do not edit below this line
