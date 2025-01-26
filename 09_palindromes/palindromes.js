640c1e6ed773411c8fe9e08c101a214b46cdf2fb
osoriogeorge <osoriovgeorge@gmail.com>
2025-01-26 14:45:31 +0100

    palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  const invertedText = text.split("").reverse().join("");
+  if (text === invertedText) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
