314e37ddab1db8a2409e90c349bfa4d52bfac10c
osoriogeorge <osoriovgeorge@gmail.com>
2025-01-27 07:40:45 +0100

    FInfind the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+const findTheOldest = function (people) {
+  return people.reduce((oldest, person) => {
+    const currentYear = new Date().getFullYear();
+    const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    const oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
 
+    return personAge > oldestAge ? person : oldest;
+  });
 };
-
+findTheOldest(people);
 // Do not edit below this line
 module.exports = findTheOldest;
